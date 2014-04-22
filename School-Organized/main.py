8ade3d093f1df5ebd9b7f0a9eeed0712bbfb4ce2
pianist1119 <pianist1119@gmail.com>
2014-04-22 18:49:45 -0400

    UI Update

--- School-Organized/main.py
+++ School-Organized/main.py
@@ -1,10 +1,28 @@
 from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import ObjectProperty
+from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelItem
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.lang import Builder
 
-class RootWidget(FloatLayout):
-    pass
-    
+class RootWidget(TabbedPanel):
+    monday = ObjectProperty(None)
+    tuesday = ObjectProperty(None)
+    wednesday = ObjectProperty(None)
+    thursday = ObjectProperty(None)
+    friday = ObjectProperty(None)
+    text1 = '1'
+    text2 = '2'
+    text3 = '3'
+    text4 = '4'
+    text5 = '5'
+    label1 = ObjectProperty(None)
+    label2 = ObjectProperty(None)
+    label3 = ObjectProperty(None)
+    label4 = ObjectProperty(None)
+    label5 = ObjectProperty(None)
 class MainApp(App):
     def build(self):
         return RootWidget()
