900fd50d9a384f9481651a0c2fc0586310c5f439
pianist1119 <pianist1119@gmail.com>
2014-04-30 10:38:06 -0400

    Update #3
    Looks like all thats left is Notifications and notes! Though i may yet
    edit the starting kv file not to include "Day/Month/Year"

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from kivy.uix.tabbedpanel import TabbedPanel
-from kivy.base import runTouchApp
+from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
 from kivy.uix.boxlayout import BoxLayout
@@ -11,6 +11,10 @@ from kivy.uix.spinner import Spinner
 
 class RootWidget(TabbedPanel):
 
+    day3 = map(str,range(1,32))
+    month = map(str,range(1,13))
+    year = map(str,range(2014,2051))
+
     opened = False
     try:
         tmp = open('data', 'r')
@@ -20,13 +24,59 @@ class RootWidget(TabbedPanel):
         data = load(tmp)
         tmp.close()
     else:
-        data = {'Monday':[['First Hour', '', '', '', '', """"""],['Second Hour', '', '', '', '', """"""],['Third Hour','','','','',""""""],['Fourth Hour','','','','',""""""],['Fifth Hour','','','','',""""""]],
-                'Tuesday':[['First Hour', '', '', '', '', """"""],['Second Hour', '', '', '', '', """"""],['Third Hour','','','','',""""""],['Fourth Hour','','','','',""""""],['Fifth Hour','','','','',""""""]],
-                'Wednesday':[['First Hour', '', '', '', '', """"""],['Second Hour', '', '', '', '', """"""],['Third Hour','','','','',""""""],['Fourth Hour','','','','',""""""],['Fifth Hour','','','','',""""""]],
-                'Thursday':[['First Hour', '', '', '', '', """"""],['Second Hour', '', '', '', '', """"""],['Third Hour','','','','',""""""],['Fourth Hour','','','','',""""""],['Fifth Hour','','','','',""""""]],
-                'Friday':[['First Hour', '', '', '', '', """"""],['Second Hour', '', '', '', '', """"""],['Third Hour','','','','',""""""],['Fourth Hour','','','','',""""""],['Fifth Hour','','','','',""""""]]}
+        data = {'Monday':[['First Hour', '', 'Month/Day/Year',
+                            '', 'Month/Day/Year', """"""],
+                          ['Second Hour', '', 'Month/Day/Year',
+                            '', 'Month/Day/Year', """"""],
+                          ['Third Hour','','Month/Day/Year',
+                           '','Month/Day/Year',""""""],
+                          ['Fourth Hour','','Month/Day/Year',
+                           '','Month/Day/Year',""""""],
+                          ['Fifth Hour','','Month/Day/Year',
+                           '','Month/Day/Year',""""""]],
+                'Tuesday':[['First Hour', '', 'Month/Day/Year',
+                             '', 'Month/Day/Year', """"""],
+                           ['Second Hour', '', 'Month/Day/Year',
+                             '', 'Month/Day/Year', """"""],
+                           ['Third Hour','','Month/Day/Year',
+                            '','Month/Day/Year',""""""],
+                           ['Fourth Hour','','Month/Day/Year',
+                            '','Month/Day/Year',""""""],
+                           ['Fifth Hour','','Month/Day/Year',
+                            '','Month/Day/Year',""""""]],
+                'Wednesday':[['First Hour', '', 'Month/Day/Year',
+                               '', 'Month/Day/Year', """"""],
+                             ['Second Hour', '', 'Month/Day/Year',
+                               '', 'Month/Day/Year', """"""],
+                             ['Third Hour','','Month/Day/Year',
+                              '','Month/Day/Year',""""""],
+                             ['Fourth Hour','','Month/Day/Year',
+                              '','Month/Day/Year',""""""],
+                             ['Fifth Hour','','Month/Day/Year',
+                              '','Month/Day/Year',""""""]],
+                'Thursday':[['First Hour', '', 'Month/Day/Year',
+                              '', 'Month/Day/Year', """"""],
+                            ['Second Hour', '', 'Month/Day/Year',
+                              '', 'Month/Day/Year', """"""],
+                            ['Third Hour','','Month/Day/Year',
+                             '','Month/Day/Year',""""""],
+                            ['Fourth Hour','','Month/Day/Year',
+                             '','Month/Day/Year',""""""],
+                            ['Fifth Hour','','Month/Day/Year',
+                             '','Month/Day/Year',""""""]],
+                'Friday':[['First Hour', '', 'Month/Day/Year',
+                            '', 'Month/Day/Year', """"""],
+                          ['Second Hour', '', 'Month/Day/Year',
+                            '', 'Month/Day/Year', """"""],
+                          ['Third Hour','','Month/Day/Year',
+                           '','Month/Day/Year',""""""],
+                          ['Fourth Hour','','Month/Day/Year',
+                           '','Month/Day/Year',""""""],
+                          ['Fifth Hour','','Month/Day/Year',
+                           '','Month/Day/Year',""""""]]}
 
     def edit(self,label,day,hour):
+
         self.label = label
         self.day = day
         self.hour = hour
@@ -36,12 +86,59 @@ class RootWidget(TabbedPanel):
         self.label3 = Label(text= 'Assignment')
         self.label4 = Label(text='Duedate')
         self.label5 = Label(text='Class Name')
-        self.spinner = Spinner(text='1',values=tuple(range(1,32),size_hint=(.25,.25)))
-        self.hour_name = TextInput(text_hint='Class Name',text=self.data[day][hour][0],multiline=False)
-        self.assignment1in = TextInput(text_hint='Assignment',text=self.data[day][hour][1],multiline=False)
-        self.assignment2in = TextInput(text_hint='Assignment',text=self.data[day][hour][3],multiline=False)
-        self.duedate1in = TextInput(text_hint='Duedate',text=self.data[day][hour][2],multiline=False)
-        self.duedate2in = TextInput(text_hint='Duedate',text=self.data[day][hour][4],multiline=False)
+
+        self.hour_name = TextInput(text_hint='Class Name',
+                                   text=self.data[day][hour][0],
+                                   multiline=False)
+        self.assignment1in = TextInput(text_hint='Assignment',
+                                       text=self.data[day][hour][1],
+                                       multiline=False)
+        self.assignment2in = TextInput(text_hint='Assignment',
+                                       text=self.data[day][hour][3],
+                                       multiline=False)
+        self.duedate1in = TextInput(text_hint='Duedate',
+                                    text=self.data[day][hour][2],
+                                    multiline=False)
+        self.duedate2in = TextInput(text_hint='Duedate',
+                                    text=self.data[day][hour][4],
+                                    multiline=False)
+
+        duedate1in = BoxLayout(orientation='horizontal')
+        duedate2in = BoxLayout(orientation='horizontal')
+        self.day1 = Spinner(text=self.data[day][hour][2].split('/')[0],
+                            values=tuple(self.day3),
+                            size_hint_x=.75,
+                            pos_hint={'center_x':.5,'center_y':.5})
+        print 1
+        self.day2 = Spinner(text=self.data[day][hour][4].split('/')[0],
+                            values=tuple(self.day3),
+                            size_hint_x=.75,
+                            pos_hint={'center_x':.5,'center_y':.5})
+        self.month1 = Spinner(text=self.data[day][hour][2].split('/')[1],
+                              values=tuple(self.month),
+                              size_hint_x=.75,
+                              pos_hint={'center_x':.5,'center_y':.5})
+        print self.data[day][hour][4].split('/')
+        self.month2 = Spinner(text=self.data[day][hour][4].split('/')[1],
+                              values=tuple(self.month),
+                              size_hint_x=.75,
+                              pos_hint={'center_x':.5,'center_y':.5})
+        self.year1 = Spinner(text=self.data[day][hour][2].split('/')[2],
+                             values=tuple(self.year),
+                             size_hint_x=.75,
+                             pos_hint={'center_x':.5,'center_y':.5})
+        self.year2 = Spinner(text=self.data[day][hour][4].split('/')[2],
+                             values=tuple(self.year),
+                             size_hint_x=.75,
+                             pos_hint={'center_x':.5,'center_y':.5})
+        print 2
+        duedate1in.add_widget(self.day1)
+        duedate1in.add_widget(self.month1)
+        duedate1in.add_widget(self.year1)
+        duedate2in.add_widget(self.day2)
+        duedate2in.add_widget(self.month2)
+        duedate2in.add_widget(self.year2)
+        print 3
         self.okbutton = Button(text='Ok')
         self.cancelbutton = Button(text='Cancel')
         self.editback.add_widget(self.label5)
@@ -49,33 +146,46 @@ class RootWidget(TabbedPanel):
         self.editback.add_widget(self.label1)
         self.editback.add_widget(self.assignment1in)
         self.editback.add_widget(self.label2)
-        self.editback.add_widget(self.duedate1in)
+        print 4
+        self.editback.add_widget(duedate1in)
         self.editback.add_widget(self.label3)
         self.editback.add_widget(self.assignment2in)
         self.editback.add_widget(self.label4)
-        self.editback.add_widget(self.duedate2in)
+        print 5
+        self.editback.add_widget(duedate2in)
+        print 6
         self.editback.add_widget(self.okbutton)
         self.editback.add_widget(self.cancelbutton)
-        self.popup = Popup(content=self.editback,auto_dismiss=False,size_hint=(.75,.75),title='Edit')
+
+        self.popup = Popup(content=self.editback,auto_dismiss=False,
+                           size_hint=(.75,.75),title='Edit')
         self.okbutton.bind(on_press=self.dismissok)
         self.cancelbutton.bind(on_press=self.popup.dismiss)
         self.popup.open()
 
     def dismissok(self,instance):
         self.data[self.day][self.hour][0] = self.hour_name.text
-        self.data[self.day][self.hour][1] = self.assignment1in.text
-        self.data[self.day][self.hour][2] = self.duedate1in.text
-        self.data[self.day][self.hour][3] = self.assignment2in.text
-        self.data[self.day][self.hour][4] = self.duedate2in.text
+        if (self.day1.text != 'Day') and (self.month1.text != 'Month') and (self.year1.text != 'Year'):
+             self.data[self.day][self.hour][1] = self.assignment1in.text
+
+        if self.assignment1in.text and (self.day1.text != 'Day') and (self.month1.text != 'Month') and (self.year1.text != 'Year'):
+             self.data[self.day][self.hour][2] = self.day1.text + '/' + self.month1.text + '/' + self.year1.text
+
+        if (self.day2.text != 'Day') and (self.month2.text != 'Month') and (self.year2.text != 'Year'):
+             self.data[self.day][self.hour][3] = self.assignment2in.text
+
+        if self.assignment2in.text and (self.day2.text != 'Day') and (self.month2.text != 'Month') and (self.year2.text != 'Year'):
+             self.data[self.day][self.hour][4] = self.duedate2in.text
+
         self.label.text = self.data['Monday'][0][0] + '\n' + self.data['Monday'][0][1] + ' ' + self.data['Monday'][0][2] + '\n' + self.data['Monday'][0][3] + ' ' + self.data['Monday'][0][4]
         data = open('data','w')
         dump(self.data,data)
         data.close()
         self.popup.dismiss()
-#
-# class MainApp(App):
-#     def build(self):
-#         return RootWidget()
+
+class MainApp(App):
+    def build(self):
+        return RootWidget()
 
 if __name__=='__main__' or '__android__':
-    runTouchApp(RootWidget)
+    MainApp().run()
